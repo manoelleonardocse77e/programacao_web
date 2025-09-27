4f7d410272bcade30e368c86b465ff3512906638
Manoel Leonardo Carneiro Sette Serrano <1232301946@acd.grupotiradentes.com>
2025-09-27 10:33:36 -0300

    feat:aula de sabado realizando a autenticação da aplicação

---
+++ views/auth.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import login_required, logout_user, login_user
+from models.user import User 
+from extensions import db 
+
+auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+@auth_bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user and user.check_password(password):
+            login_user(user)
+            flash("Login realizado com sucesso!", "success")
+            return redirect (url_for("dashboard"))
+        else:
+            flash("Credenciais inválidas.", "danger")
+    return render_template("auth/login.html")
